9666356144cc3fd8ebdea6a543b0716fe2c572fd
adityadevcode <dev.adityacode@gmail.com>
2022-11-29 14:43:35 -0500

    test commit

---
+++ sample.js
@@ -0,0 +1,15 @@
+let name = "color";
+console.log(name);
+
+// js notes pt1
+// primitive data types: string/integer/boolean/undefined/null
+//reference type:objects/functions/array
+//we can modify values in array by using const
+//objs and array are ref type
+// ref types cam always modified
+// es5 we only using var
+//es6 let and const
+
+// boolean
+let wed="true";
+console.log(wed);
